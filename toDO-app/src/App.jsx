c8689c49f8984b80fca16c938ae6cd4cc72e43da
iamcardini <petergodswill1100@gmail.com>
2024-07-26 16:22:41 +0100

    first commit

---
+++ toDO-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './index.css'
+import TodoApp from './TodoApp'
+
+function App() {
+
+  return (
+    <div>
+      <TodoApp/>
+    </div>
+  )
+}
+
+export default App
---
+++ toDO-app/src/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoApp = () => {
+  return (
+    <div>
+       <h1 className="bg-gray-700 p-2 text-white cursor-pointer w-full">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default TodoApp
